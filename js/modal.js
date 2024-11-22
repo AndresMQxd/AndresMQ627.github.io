4f8a2ec8766907e60d02ce90ab25936f1c999685
AndresMQ627 <andres.190822@gmail.com>
2024-11-21 23:40:38 -0600

    a

--- js/modal.js
+++ js/modal.js
@@ -19,3 +19,36 @@ document.getElementById("btnSave").onclick =(evt)=>{
       });
 
 }
+
+
+
+const buttons = document.querySelectorAll('.custom-btn');
+
+// Agregar un evento a cada botón
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('borrarTanda')) {
+      Swal.fire({
+        title: "Estas seguro que desea eliminar esta tanda?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        cancelButtonText: "Cancelar",
+        confirmButtonText: "Si, borrar!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Tanda eliminada!",
+    
+            icon: "success"
+          });
+        }
+      });
+    } else if(button.classList.contains('editarTanda')){
+      alert('¡Hiciste clic en editar tanda!');
+    }else{
+      alert('¡Hiciste clic en ver particpantes!');
+    }
+  });
+});
